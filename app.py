5ad0256b4334b71e875db947b3f2684719d2b0a1
Zakk0102 <141924599+Zakk0102@users.noreply.github.com>
2024-04-19 15:04:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def authorize_credentials():
 
 
 
-if __name__ == '__main__':
-   app.run(debug=True, host="0.0.0.0", port=5001)
+# if __name__ == '__main__':
+#    app.run(debug=True, host="0.0.0.0", port=5001)
